015e69d649712b48f372f7b39d640acc2d8f69ce
CoderDeepasnhu <d54013768@gmail.com>
2025-04-06 16:17:22 +0530

    chnage the files

--- mypro/mypro/settings.py
+++ mypro/mypro/settings.py
@@ -9,14 +9,12 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_URL='/static/'
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'myapp','static')]
-STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -28,7 +26,7 @@ SECRET_KEY = 'django-insecure-!p16k5dy5(#8q1+krkon)9v^dw83yg@o^&$ev(#^9jg!)+6-k4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -55,9 +53,6 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'mypro.urls'
 import os
- 
-
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
