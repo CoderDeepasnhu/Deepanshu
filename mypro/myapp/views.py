86bd67c314cc58bb6e070299fd5b22acb719aa60
CoderDeepasnhu <d54013768@gmail.com>
2025-04-06 10:44:55 +0530

    add new files

---
+++ mypro/myapp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def get_name(request):
+    name=""
+    if request.method=="POST":
+        name=request.POST.get("name")
+    return render(request,'index.html',{"name":name})
